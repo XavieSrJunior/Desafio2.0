88f6f34b5b21d3dafdf975b11828fc1373c2fe68
XavierSrjunior <xaviersjunior@gmail.com>
2021-04-03 18:14:31 -0300

    implementação botao ednona

--- templates/android-dot-com/desafio2.js
+++ templates/android-dot-com/desafio2.js
@@ -1,11 +1,18 @@
-//Desafio 2.0
 
-//CAPTURANDO O ELEMENTO HTML USANDO UM ATRIBUTO AO INVÉS DA CLASSE PARA PRATICAR O USO DO DATA ATTRIBUTE:
-var logo = document.querySelector(".android-logo-image");
+// estou substituindo a classe original pela variável Ednona para facilitar a visualização e implementação//
+var Ednona = document.querySelector(".mdl-navigation__link mdl-typography--text-uppercase")  
 
-//ADICIONANDO UM ESCUTADOR NO ELEMENTO DO BOTÃO VIEW SOURCE, QUE AO SER CLICADO IRÁ COLOCAR A VARIÁVEL E O EVENTO NO DATALAYER:
-logo.addEventListener("click", function(){
-	dataLayer.push({
-	'event': 'eventoTeste',
+
+//criando um escutador de eventos que é acionado quando a variável Ednona recebe um click//
+Ednona.addEventListener("click", function(){
+
+	window.dataLayer = window.dataLayer || [];
+	//esse código preenche o dataLayer com os valores descritos no objeto//
+	window.dataLayer.push({
+	  'event': 'generic-event',
+	  'eventCategory': ' android:header ',
+	  'eventAction': ' clique:header:menu ',
+	  'eventLabel': ' ednona '
 	});
-});
+
+}
