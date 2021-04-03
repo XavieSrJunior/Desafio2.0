3705134c660ac0daf63f83ce208fc747cc15e13b
XavierSrjunior <xaviersjunior@gmail.com>
2021-04-03 19:02:03 -0300

    implementando todos os botões menu-header

--- templates/android-dot-com/desafio2.js
+++ templates/android-dot-com/desafio2.js
@@ -1,18 +1,108 @@
 
 // estou substituindo a classe original pela variável Ednona para facilitar a visualização e implementação//
-var Ednona = document.querySelector(".mdl-navigation__link.mdl-typography--text-uppercase")  
+var ednona = document.querySelector(".mdl-navigation__link.mdl-typography--text-uppercase")  
 
 
 //criando um escutador de eventos que é acionado quando a variável Ednona recebe um click//
-Ednona.addEventListener("click", function(){
+ednona.addEventListener("click", function(){
 
 	window.dataLayer = window.dataLayer || [];
 	//esse código preenche o dataLayer com os valores descritos no objeto//
 	window.dataLayer.push({
 	  'event': 'generic-event',
-	  'eventCategory': ' android:header ',
-	  'eventAction': ' clique:header:menu ',
-	  'eventLabel': ' ednona '
+	  'eventCategory': 'android:header',
+	  'eventAction': 'clique:header:menu',
+	  'eventLabel': 'ednona'
+	});
+
+});
+
+var tablets = document.querySelectorAll('.mdl-navigation__link.mdl-typography--text-uppercase')[1]
+
+tablets.addEventListener("click", function(){
+
+	window.dataLayer = window.dataLayer || [];
+	//esse código preenche o dataLayer com os valores descritos no objeto//
+	window.dataLayer.push({
+	  'event': 'generic-event',
+	  'eventCategory': 'android:header',
+	  'eventAction': 'clique:header:menu',
+	  'eventLabel': 'tablets'
+	});
+
+});
+
+var wear = document.querySelectorAll('.mdl-navigation__link.mdl-typography--text-uppercase')[2]
+
+wear.addEventListener("click", function(){
+
+	window.dataLayer = window.dataLayer || [];
+	//esse código preenche o dataLayer com os valores descritos no objeto//
+	window.dataLayer.push({
+	  'event': 'generic-event',
+	  'eventCategory': 'android:header',
+	  'eventAction': 'clique:header:menu',
+	  'eventLabel': 'wear'
+	});
+
+});
+
+var tv = document.querySelectorAll('.mdl-navigation__link.mdl-typography--text-uppercase')[3]
+
+tv.addEventListener("click", function(){
+
+	window.dataLayer = window.dataLayer || [];
+	//esse código preenche o dataLayer com os valores descritos no objeto//
+	window.dataLayer.push({
+	  'event': 'generic-event',
+	  'eventCategory': 'android:header',
+	  'eventAction': 'clique:header:menu',
+	  'eventLabel': 'tv'
+	});
+
+});
+
+var auto = document.querySelectorAll('.mdl-navigation__link.mdl-typography--text-uppercase')[4]
+
+auto.addEventListener("click", function(){
+
+	window.dataLayer = window.dataLayer || [];
+	//esse código preenche o dataLayer com os valores descritos no objeto//
+	window.dataLayer.push({
+	  'event': 'generic-event',
+	  'eventCategory': 'android:header',
+	  'eventAction': 'clique:header:menu',
+	  'eventLabel': 'auto'
+	});
+
+});
+
+var one = document.querySelectorAll('.mdl-navigation__link.mdl-typography--text-uppercase')[5]
+
+one.addEventListener("click", function(){
+
+	window.dataLayer = window.dataLayer || [];
+	//esse código preenche o dataLayer com os valores descritos no objeto//
+	window.dataLayer.push({
+	  'event': 'generic-event',
+	  'eventCategory': 'android:header',
+	  'eventAction': 'clique:header:menu',
+	  'eventLabel': 'one'
+	});
+
+});
+
+var play = document.querySelectorAll('.mdl-navigation__link.mdl-typography--text-uppercase')[6]
+
+play.addEventListener("click", function(){
+
+	window.dataLayer = window.dataLayer || [];
+	//esse código preenche o dataLayer com os valores descritos no objeto//
+	window.dataLayer.push({
+	  'event': 'generic-event',
+	  'eventCategory': 'android:header',
+	  'eventAction': 'clique:header:menu',
+	  'eventLabel': 'play'
 	});
 
 });
